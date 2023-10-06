5f959cf681925421943d62d36ebb7ce40542957a
John <mjohncorbet@gmail.com>
2023-10-06 18:56:53 +0800

    create gameboard class

---
+++ src/gameboard.ts
@@ -0,0 +1,24 @@
+function setupBoard() {
+    const board: string[][] = [];
+    const firstRow = '0ABCDEFGHIJ';
+    board.push(firstRow.split(''));
+
+    for(let i = 1; i <= 10; i++) {
+        const squares = '0..........';
+        const row = squares.split('');
+        row[0] = i.toString();
+        board.push(row);
+    }
+
+    return board;
+}
+
+export default class GameBoard {
+    board: string[][];
+    ships: object;
+
+    constructor() {
+        this.board = setupBoard();
+        this.ships = {};
+    }
+}
