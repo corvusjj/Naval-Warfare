43b5995c2c23f9217baa40049142917356cc1304
John <mjohncorbet@gmail.com>
2023-10-07 22:56:18 +0800

    configure lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,15 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended', 
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: './',
+  },
   root: true,
 };
