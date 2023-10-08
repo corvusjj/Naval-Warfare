990528528f9710e1dcb80cdffe2937d1b0a244cb
John <mjohncorbet@gmail.com>
2023-10-08 17:23:04 +0800

    setup ship placement tests

--- src/tests/gameboard.test.ts
+++ src/tests/gameboard.test.ts
@@ -1,5 +1,6 @@
 import { expect, it, describe } from "vitest";
 import GameBoard from "../gameboard";
+import { setupBoard } from "../gameboard";
 import Ship from "../ship";
 
 const boardTemplate = [
@@ -24,7 +25,7 @@ describe('Board coordinates', () => {
 });
 
 //  placing ship coordinates are arranged (up to down), (left to right) starting from initial square.
-describe('Single Ship Placement', () => {
+describe('Seek Placement (single ship)', () => {
     it('vertical ship(3) on coordinates 0-3 should be true with returning coordinates [0,3], [1,3], [2,3].', () => {
         const gameBoard = new GameBoard();
         const cruiser = new Ship(3);
@@ -51,5 +52,56 @@ describe('Single Ship Placement', () => {
         expect(gameBoard.seekCoordinates(battleship, '2-7')).toEqual(
             { canBePlaced:true, coordinates:[[2,7], [2,8], [2,9], [2,10]] }
         );
+    });
+});
+
+describe('Marking ship characters on board', () => {
+    it('vertical battleship(4) on coordinates 7-4 should be equal to modified board.', () => {
+        const modifiedBoard = setupBoard();
+        modifiedBoard[7][4] = 'b';
+        modifiedBoard[8][4] = 'b';
+        modifiedBoard[9][4] = 'b';
+        modifiedBoard[10][4] = 'b';
+
+        const gameBoard = new GameBoard();
+        const battleship = new Ship(4);
+        gameBoard.placeShip(battleship, '7-4', 'b');
+
+        expect(gameBoard.getBoard()).toEqual(modifiedBoard);
+    });
+
+    it('horizontal destroyer(2) on coordinates 8-7 and vertical submarine(3) on coordinates 2-3 should be equal to modified board.', () => {
+        const modifiedBoard = setupBoard();
+        modifiedBoard[8][7] = 'd';
+        modifiedBoard[8][8] = 'd';
+        modifiedBoard[2][3] = 's';
+        modifiedBoard[3][3] = 's';
+        modifiedBoard[4][3] = 's';
+
+        const gameBoard = new GameBoard();
+        const destroyer = new Ship(2);
+        destroyer.toggleDirection();
+        const submarine = new Ship(3);
+        gameBoard.placeShip(destroyer, '8-7', 'd');
+        gameBoard.placeShip(submarine, '2-3', 's');
+
+        expect(gameBoard.getBoard()).toEqual(modifiedBoard);
+    });
+
+    it('horizontal cruiser(3) on coordinates 3-3 will not be marked on board after placing vertical battleship(4) on coordinates 2-5', () => {
+        const modifiedBoard = setupBoard();
+        modifiedBoard[2][5] = 'b';
+        modifiedBoard[3][5] = 'b';
+        modifiedBoard[4][5] = 'b';
+        modifiedBoard[5][5] = 'b';
+
+        const gameBoard = new GameBoard();
+        const battleship = new Ship(4);
+        const cruiser = new Ship(3);
+        cruiser.toggleDirection();
+        gameBoard.placeShip(battleship, '2-5', 'b');
+        gameBoard.placeShip(cruiser, '3-3', 'r');
+
+        expect(gameBoard.getBoard()).toEqual(modifiedBoard);
     });
 });
