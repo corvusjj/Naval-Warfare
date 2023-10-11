9daa6886849cbe2bb74c2bde92c63afc45782d4b
John <mjohncorbet@gmail.com>
2023-10-11 14:12:31 +0800

    setup beforeEach on tests

--- src/tests/gameboard.test.ts
+++ src/tests/gameboard.test.ts
@@ -1,4 +1,4 @@
-import { expect, it, describe } from "vitest";
+import { expect, it, describe, beforeEach } from "vitest";
 import GameBoard from "../gameboard";
 import { setupBoard } from "../gameboard";
 import Ship from "../ship";
@@ -15,55 +15,69 @@ const boardTemplate = [
     ['8', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',],
     ['9', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',],
     ['10', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',],
-]
+];
+
+let gameBoard: GameBoard;
+
+beforeEach(() => {
+    gameBoard = new GameBoard();
+});
 
 describe('Board coordinates', () => {
     it('gameBoard instance should be equal to boardTemplate.', () => {
-        const x = new GameBoard();
-        expect(x.getBoard()).toEqual(boardTemplate);
+        expect(gameBoard.getBoard()).toEqual(boardTemplate);
     });
 });
 
 //  placing ship coordinates are arranged (up to down), (left to right) starting from initial square.
-describe('Seek Placement (single ship)', () => {
+describe('Seek ship placement', () => {
     it('vertical ship(3) on coordinates 0-3 should be true with returning coordinates [0,3], [1,3], [2,3].', () => {
-        const gameBoard = new GameBoard();
         const cruiser = new Ship(3);
-
         expect(gameBoard.seekCoordinates(cruiser, '0-3')).toEqual(
             { canBePlaced:true, coordinates:[[0,3], [1,3], [2,3]] }
         );
     });
 
     it('vertical ship(5) on coordinates 8-10 should be false with returning coordinates [8,10], [9,10], [10,10].', () => {
-        const gameBoard = new GameBoard();
         const carrier = new Ship(5);
-
         expect(gameBoard.seekCoordinates(carrier, '8-10')).toEqual(
             { canBePlaced:false, coordinates:[[8,10], [9,10], [10,10]] }
         );
     });
 
     it('horizontal ship(4) on coordinates 2-7 should be true with returning coordinates [2,7], [2,8], [2,9], [2,10].', () => {
-        const gameBoard = new GameBoard();
         const battleship = new Ship(4);
         battleship.toggleDirection();
-
         expect(gameBoard.seekCoordinates(battleship, '2-7')).toEqual(
             { canBePlaced:true, coordinates:[[2,7], [2,8], [2,9], [2,10]] }
+        );
+    });
+
+    it('vertical ship(5) on coordinates 2-9 after placing horizontal ship(2) on coordinates 3-9 should be false with returning coordinates [2,9].', () => {
+        const destroyer = new Ship(2);
+        destroyer.toggleDirection();
+        gameBoard.placeShip(destroyer, '3-9', 'd');
+        const carrier = new Ship(5);
+
+        expect(gameBoard.seekCoordinates(carrier, '2-9')).toEqual(
+            { canBePlaced:false, coordinates:[[2,9]] }
         );
     });
 });
 
 describe('Marking ship characters on board', () => {
+    let modifiedBoard: string[][];
+
+    beforeEach(() => {
+        modifiedBoard = setupBoard();
+    });
+
     it('vertical battleship(4) on coordinates 7-4 should be equal to modified board.', () => {
-        const modifiedBoard = setupBoard();
         modifiedBoard[7][4] = 'b';
         modifiedBoard[8][4] = 'b';
         modifiedBoard[9][4] = 'b';
         modifiedBoard[10][4] = 'b';
 
-        const gameBoard = new GameBoard();
         const battleship = new Ship(4);
         gameBoard.placeShip(battleship, '7-4', 'b');
 
@@ -71,14 +85,12 @@ describe('Marking ship characters on board', () => {
     });
 
     it('horizontal destroyer(2) on coordinates 8-7 and vertical submarine(3) on coordinates 2-3 should be equal to modified board.', () => {
-        const modifiedBoard = setupBoard();
         modifiedBoard[8][7] = 'd';
         modifiedBoard[8][8] = 'd';
         modifiedBoard[2][3] = 's';
         modifiedBoard[3][3] = 's';
         modifiedBoard[4][3] = 's';
 
-        const gameBoard = new GameBoard();
         const destroyer = new Ship(2);
         destroyer.toggleDirection();
         const submarine = new Ship(3);
@@ -89,13 +101,11 @@ describe('Marking ship characters on board', () => {
     });
 
     it('horizontal cruiser(3) on coordinates 3-3 will not be marked on board after placing vertical battleship(4) on coordinates 2-5', () => {
-        const modifiedBoard = setupBoard();
         modifiedBoard[2][5] = 'b';
         modifiedBoard[3][5] = 'b';
         modifiedBoard[4][5] = 'b';
         modifiedBoard[5][5] = 'b';
 
-        const gameBoard = new GameBoard();
         const battleship = new Ship(4);
         const cruiser = new Ship(3);
         cruiser.toggleDirection();
--- src/tests/ship.test.ts
+++ src/tests/ship.test.ts
@@ -1,9 +1,14 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import Ship from '../ship';
 
 describe('Carrier (length of 5)', () => {
+    let carrier: Ship;
+
+    beforeEach(() => {
+        carrier = new Ship(5);
+    });
+
     it('hit 2 times, not sunk', () => {
-        const carrier = new Ship(5);
         carrier.hit();
         carrier.hit();
         expect(carrier.numOfHits()).toBe(2);
@@ -11,7 +16,6 @@ describe('Carrier (length of 5)', () => {
     });
 
     it('hit 5 times, sunk', () => {
-        const carrier = new Ship(5);
         for(let i = 0; i < 5; i++) {
             carrier.hit();
         }
@@ -20,20 +24,23 @@ describe('Carrier (length of 5)', () => {
     });
 
     it('should return "horizontal" from invoking toggleDirection method once', () => {
-        const carrier = new Ship(5);
         carrier.toggleDirection();
         expect(carrier.getDirection()).toBe('horizontal');
     });
 
     it('should return "vertical" as direction by default', () => {
-        const carrier = new Ship(5);
         expect(carrier.getDirection()).toBe('vertical');
     })
 });
 
 describe('Cruiser (length of 3)', () => {
+    let cruiser: Ship;
+
+    beforeEach(() => {
+        cruiser = new Ship(3);
+    });
+
     it('hit 3 times, sunk', () => {
-        const cruiser = new Ship(3);
         for(let i = 0; i < 3; i++) {
             cruiser.hit();
         }
@@ -42,15 +49,13 @@ describe('Cruiser (length of 3)', () => {
     });
 
     it('num of hits should be equal to 0 if not hit', () => {
-        const cruiser = new Ship(3);
         expect(cruiser.numOfHits()).toBe(0);
     });
     
     it('should return "vertical" from invoking toggleDirection method twice', () => {
-        const carrier = new Ship(3);
-        carrier.toggleDirection();
-        carrier.toggleDirection();
+        cruiser.toggleDirection();
+        cruiser.toggleDirection();
         
-        expect(carrier.getDirection()).toBe('vertical');
+        expect(cruiser.getDirection()).toBe('vertical');
     });
 });
