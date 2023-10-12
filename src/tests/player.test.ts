6793850b274d684d4f4608d3cac2053d813cebd1
John <mjohncorbet@gmail.com>
2023-10-12 21:21:04 +0800

    setup player test

---
+++ src/tests/player.test.ts
@@ -0,0 +1,23 @@
+import { expect, it, describe, beforeEach } from "vitest";
+import Player from "../player";
+
+let player1: Player;
+let player2: Player;
+
+beforeEach(() => {
+    player1 = new Player();
+    player2 = new Player();
+
+    player2.gameBoard.placeShip(
+        player2.ships.b, '1-7','b'
+    );
+});
+
+it('should attack another player', () => {
+    player1.attack([1,7], player2);
+    
+    expect(player2.gameBoard.getShipsStatus().b).toEqual({
+        hits: 1,
+        sunk: false
+    });
+});
