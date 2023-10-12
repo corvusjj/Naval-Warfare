290be724a1a1bcccebf0f991f352160a12290a5e
John <mjohncorbet@gmail.com>
2023-10-12 21:22:02 +0800

    setup player class

---
+++ src/player.ts
@@ -0,0 +1,31 @@
+import GameBoard from "./gameboard";
+import Ship from "./ship";
+type playerShips = Record<string, Ship>;
+
+function setupShips() {
+    return {
+        c: new Ship(5),  // carrier
+        b: new Ship(4),  // battleship
+        r: new Ship(3),  // cruiser
+        s: new Ship(3),  // submarines
+        d: new Ship(2)   // destroyer 
+    }
+}
+
+export default class Player {
+    gameBoard: GameBoard;
+    ships: playerShips;
+
+    constructor() {
+        this.gameBoard = new GameBoard;
+        this.ships = setupShips();
+    }
+
+    receiveAttack(square:number[]) {
+        this.gameBoard.receiveAttack(square);
+    }
+
+    attack(square:number[], opponent:Player) {
+        opponent.receiveAttack(square);
+    }
+}
