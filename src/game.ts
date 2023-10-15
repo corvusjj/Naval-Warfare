296e93145e52c0421d674cb865ae834421731a94
John <mjohncorbet@gmail.com>
2023-10-15 22:46:19 +0800

    setup startGame function

--- src/game.ts
+++ src/game.ts
@@ -1,13 +1,16 @@
 import Player from './player';
 import { setupBoardUI } from './components/interface';
 
-function createPlayer() {
-    return new Player;
-}
+function startGame(vsComputer:boolean, playerNames:string[]) {
+    const p1 = new Player(playerNames[0]);
+    let p2:Player;
 
-function startGame() {
-    setupBoardUI('p1');
-    setupBoardUI('p2');
+    vsComputer?
+    p2 = new Player('Fleet Admiral Bot'):
+    p2 = new Player(playerNames[1]);
+
+    setupBoardUI(p1.name);
+    setupBoardUI(p2.name);
 }
 
 export { startGame }
