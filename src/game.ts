670b4743a59f7195300139086ad4f4e81a4e577c
John <mjohncorbet@gmail.com>
2023-10-16 23:21:02 +0800

    setup userMethods for interface

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,9 @@
 import Player from './player';
-import { setupBoardUI } from './components/interface';
+import { 
+    toggleBoardUI
+} from './components/interface';
+
+let gameState: GameState;
 
 class GameState {
     vsComputer: boolean;
@@ -24,6 +28,8 @@ class GameState {
             this.attacker = this.p1;
             this.defender = this.p2;
         }
+
+        this.attacker === this.p1? toggleBoardUI(0): toggleBoardUI(1);
     }
 
     attack(square: number[]) {
@@ -31,16 +37,23 @@ class GameState {
     }   
 }
 
-function startGame(vsComputer:boolean, playerNames:string[]) {
-    const p1 = new Player(playerNames[0]);
-    let p2:Player;
+const userMethods = {
+    setGameState: (vsComputer:boolean, playerNames:string[]) => {
+        const p1 = new Player(playerNames[0]);
+        let p2:Player;
+    
+        vsComputer?
+        p2 = new Player('Fleet Admiral Bot'):
+        p2 = new Player(playerNames[1]);
+        
+        gameState = new GameState(p1, p2);
+        if (vsComputer) gameState.vsComputer = true;
+        console.log(gameState);
+    },
 
-    vsComputer?
-    p2 = new Player('Fleet Admiral Bot'):
-    p2 = new Player(playerNames[1]);
-
-    setupBoardUI(p1.name);
-    setupBoardUI(p2.name);
+    attack: (square: number[]) => {
+        gameState.attack(square);
+    }
 }
 
-export { GameState, startGame }
+export { userMethods }
