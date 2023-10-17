9f752231d8a13b26b04086f748f880542cf78844
John <mjohncorbet@gmail.com>
2023-10-17 23:01:14 +0800

    setup enemySquares coordinates for ai

--- src/components/aiPlayer.ts
+++ src/components/aiPlayer.ts
@@ -1,7 +1,26 @@
 import Player from '../player';
 
+function setupAllCoordinates() {
+    const coordinates: number[][] = [];
+    let [x, y] = [1, 1];
+
+    for (let i = 0; i < 100; i++) {
+        if (y > 10) {
+            x++;
+            y = 1;
+        }
+        coordinates.push([x, y]);
+        y++;
+    }
+
+    return coordinates;
+}
+
 export default class AiPlayer extends Player {
-    computerAttack() {
-        console.log('attack');
+    enemySquares: number[][];
+
+    constructor(name:string) {
+        super(name);
+        this.enemySquares = setupAllCoordinates();
     }
 }
