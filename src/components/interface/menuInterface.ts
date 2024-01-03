cec90ee229053009b8192fdc2cab50c7aa831e4b
John <mjohncorbet@gmail.com>
2024-01-03 16:29:33 +0800

    implement player-name submission

--- src/components/interface/menuInterface.ts
+++ src/components/interface/menuInterface.ts
@@ -2,11 +2,15 @@ const chooseOpponentBtn = document.querySelectorAll<HTMLButtonElement>('.btn-opp
 const inputNameModal = document.querySelector<HTMLDialogElement>('#input-name-modal')!;
 const firstInputLabel = inputNameModal.querySelector<HTMLLabelElement>('#name-p1 label')!;
 const secondPlayerDiv = inputNameModal.querySelector<HTMLDivElement>('#name-p2')!;
+const submitBtn = inputNameModal.querySelector<HTMLButtonElement>('#submit-btn');
+
+let singlePlayerMode = true;
 
 function singlePlayer() {
     if (secondPlayerDiv) {
         firstInputLabel.textContent = 'What\'s your name, Captain?';
-        secondPlayerDiv.style.display = 'none'; 
+        secondPlayerDiv.style.display = 'none';
+        singlePlayerMode = true; 
     }
 }
 
@@ -14,6 +18,7 @@ function multiPlayer() {
     if (secondPlayerDiv) {
         firstInputLabel.textContent = 'Player-1 Name';
         secondPlayerDiv.style.display = 'block';
+        singlePlayerMode = false;
     }
 }
 
@@ -23,10 +28,29 @@ function openInputModal(e: Event) {
     inputNameModal.showModal();
 }
 
-chooseOpponentBtn.forEach(btn => {
-    btn.addEventListener('click', openInputModal);
-});
+function submitName(e:Event) {
+    e.preventDefault();
 
-inputNameModal?.addEventListener('click', (e) => {
-    if (e.target === inputNameModal) inputNameModal.close();
-});
+    const firstInput = inputNameModal.querySelector<HTMLInputElement>('#name-p1 input')!;
+    const secondInput = inputNameModal.querySelector<HTMLInputElement>('#name-p2 input')!;
+
+    if (firstInput.value.length < 1) return;
+    if (singlePlayerMode === false && secondInput.value.length < 1) return;
+
+    sessionStorage.setItem('battleshipPlayerNames', JSON.stringify([firstInput.value, secondInput.value]));
+    window.location.href = './src/pages/placement.html';
+}
+
+export function initialize() {
+    chooseOpponentBtn.forEach(btn => {
+        btn.addEventListener('click', openInputModal);
+    });
+    
+    submitBtn?.addEventListener('click', submitName);
+    
+    inputNameModal?.addEventListener('click', (e) => {
+        if (e.target === inputNameModal) inputNameModal.close();
+    });
+}   
+
+// export { initialize }  
---
+++ src/init/menuInit.ts
@@ -0,0 +1,3 @@
+import { initialize } from '../components/interface/menuInterface';
+
+initialize();
