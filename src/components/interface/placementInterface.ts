bbb9b79dcf700227ec64caf9f8a61e89ffcd76e1
John <mjohncorbet@gmail.com>
2024-01-05 20:48:43 +0800

    style & setup ship direction toggling

---
+++ src/components/interface/placementInterface.ts
@@ -0,0 +1,36 @@
+import generateBoard from '../../utilities/battleshipBoardInterface';
+import '../style/placement.scss';
+
+const ships= document.querySelectorAll('.ship');
+
+const boardsPanel = document.querySelector('.boards-panel')!;
+let board1: HTMLDivElement;
+let board2: HTMLDivElement;
+
+function setupBoardGame() {
+    boardsPanel.innerHTML = '';
+
+    board1 = generateBoard();
+    board2 = generateBoard();
+
+    board1.setAttribute('data-index', '0');
+    board2.setAttribute('data-index', '1');
+    boardsPanel.appendChild(board1);
+    boardsPanel.appendChild(board2);
+
+    const squares = document.querySelectorAll('.square[data-coord]');
+}
+
+
+function toggleShipDirection() {
+    this.dataset.vertical === 'false'?
+    this.setAttribute('data-vertical', 'true'):
+    this.setAttribute('data-vertical', 'false');
+}
+
+export function initialize() {
+    setupBoardGame();
+    ships.forEach(ship => {
+        ship.addEventListener('click', toggleShipDirection);
+    });
+}
