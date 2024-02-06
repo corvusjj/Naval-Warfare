fa4a223ed7d53e65522cbc350e2f0169a7e54ee9
John <mjohncorbet@gmail.com>
2024-02-06 17:07:17 +0800

    initial pseudocode for ai chooseTarget

--- src/gameTemplates/aiPlayer.ts
+++ src/gameTemplates/aiPlayer.ts
@@ -61,3 +61,39 @@ export default class AiPlayer extends Player {
         return target;
     }
 }
+
+
+
+//  Algorithm for choosing a target
+
+//  let potentialNextTarget;
+
+//  1. get the highest and lowest length of unsunk enemy ship.
+//  2. state the object 'seeker' with the properties focusedTarget and directions: [vertical, horizontal];
+//      A. apply a property 'currentDirection'.
+//      B. apply a method 'reset'. ( Before executing any of the number 3, this object should be reset).
+
+//  function damagedShipTargetPursuit => should return a coordinate as potentialNextTarget based from the arguments: (direction, focusedTarget)
+
+//  3.1. if hitQueue isn't empty, choose the first coordinate as the focusedTarget and scan 
+//       each of the 4 directions (maximum squares of the highest ship length) for linear hits. 
+//          A. if there's a linear hit, choose that direction and check if placement is possible with the lowest ship length.
+//              a. if placement is possible, choose that direction and proceed to c.
+//              b. otherwise, choose the other direction.
+//              c. run damagedShipTargetPursuit and proceed to 4.
+//          B. otherwise, choose a direction and check if placement is possible with the lowest ship length.
+//              a. if placement is possible, choose that direction and proceed to c.
+//              b. otherwise, choose the other direction.
+//              c. run damagedShipTargetPursuit and proceed to 4.
+
+//  3.2. otherwise, choose a coordinate from the diagonalCoordinates as the focusedTarget.
+//          A. If a direction is available, choose one and check if placement is possible with the lowest ship length.
+//              a. if placement is possible, let focusedTarget as potentialNextTarget and proceed to 4.
+//              b. if not, eliminate the direction used and go back to A.
+//          B. If both directions are used, eliminate all the scanned coordinates including the focusedTarget. Go back to 3.2
+
+//  4. return potentialNextTarget;
+
+
+
+//  figure out checkPlacement 
