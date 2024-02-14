ed7827aaf83b701dc97e4d4d6686937594ef6dd7
John <mjohncorbet@gmail.com>
2024-02-14 16:56:17 +0800

    implement image carousel

--- src/components/interface/menuInterface.ts
+++ src/components/interface/menuInterface.ts
@@ -10,6 +10,8 @@ const inputNameModal = document.querySelector<HTMLDialogElement>('#input-name-mo
 const firstInputLabel = inputNameModal.querySelector<HTMLLabelElement>('#name-p1 label')!;
 const secondPlayerDiv = inputNameModal.querySelector<HTMLDivElement>('#name-p2')!;
 const submitBtn = inputNameModal.querySelector<HTMLButtonElement>('#submit-btn');
+const imageContainer = document.querySelector<HTMLDivElement>('.image-container')!;
+const carouselBtn = document.querySelectorAll<HTMLButtonElement>('.carousel-btn');
 
 let singlePlayerMode = true;
 
@@ -55,6 +57,42 @@ function submitName(e:Event) {
     window.location.href = './src/pages/placement.html';
 }
 
+const carouselImages:HTMLImageElement[] = [];
+
+function switchImage(e:MouseEvent) {
+    const activeImage = document.querySelector('img[data-active]');
+    let imgIndex = carouselImages.findIndex(img => img === activeImage);
+
+    function previousIndex() {
+        imgIndex -= 1;
+        if (imgIndex < 0) imgIndex = carouselImages.length - 1;
+    }
+
+    function nextIndex() {
+        imgIndex += 1;
+        if (imgIndex > carouselImages.length - 1) imgIndex = 0;
+    }
+    
+    const btn = e.target as HTMLButtonElement;
+    btn.dataset.switch === 'next'? nextIndex(): previousIndex();
+    
+    carouselImages[imgIndex].setAttribute('data-active', '');
+    activeImage?.removeAttribute('data-active');
+}
+
+function loadImages() {
+    const imgNames = ['activist-1', 'activist-2', 'navy-3', 'map', 'navy-2', 'navy-1', 'prime-ministers'];
+
+    for (const [index, name] of imgNames.entries()) {
+        const img = document.createElement('img');
+        img.src = `../../images/${name}.webp`;
+        imageContainer.appendChild(img);
+
+        carouselImages.push(img);
+        if (index === 0) img.setAttribute('data-active', '');
+    }
+}
+
 export function initialize() {
     chooseOpponentBtn.forEach(btn => {
         btn.addEventListener('click', openInputModal);
@@ -65,4 +103,7 @@ export function initialize() {
     inputNameModal?.addEventListener('click', (e) => {
         if (e.target === inputNameModal) inputNameModal.close();
     });
+
+    carouselBtn.forEach(btn => btn.addEventListener('click', switchImage));
+    window.addEventListener('load', loadImages);
 }   
